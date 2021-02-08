8ec5ba8cc3df93f71a5a86c3896352861ed53e48
Rarisma <49795711+Rarisma@users.noreply.github.com>
2021-02-08 11:02:53 +0000

    Added game view and reworked updater

--- YetAnotherGenericRougelikeGame/YetAnotherGenericRougelikeGame/MainPage.xaml.cs
+++ YetAnotherGenericRougelikeGame/YetAnotherGenericRougelikeGame/MainPage.xaml.cs
@@ -16,48 +16,33 @@ namespace YetAnotherGenericRougelikeGame
             InitializeComponent();
         }
 
-        async private void Initalise(object sender, EventArgs e) //Checks for critical data
+        async void Play(object sender, EventArgs e)
         {
-            PlayButton.Text = "Checking data folder...";
-            if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "//Data"))
-            {
-                PlayButton.Text = "Found" + FileSystem.AppDataDirectory + "//Data";
-                ResouceUpdater();
-            }
-            else
+            string UpdaterStatus = "Initalised";
+            try// These seem to fail on UWP (Win10/Xbox)
             {
                 await Permissions.CheckStatusAsync<Permissions.StorageRead>();
                 await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-                PlayButton.Text = FileSystem.AppDataDirectory + "//Data Doesn't exist!";
-                Directory.CreateDirectory(FileSystem.AppDataDirectory + "//Data");
-                await Task.Delay(100);
-                PlayButton.Text = "Made Data directory";
-                await Task.Delay(100);
-                ResouceUpdater();
             }
-        }
+            catch {Task.Delay(0);}
 
-        async void ResouceUpdater()
-        {
             PlayButton.Text = "Attempting to update...";
-            Thread.Sleep(1);
-            try //Above downloads the Resouces.Zip from GitHub
-            {
-                using (var client = new WebClient()) { client.DownloadFile("https://raw.githubusercontent.com/Rarisma/Yet-Another-Generic-Rougelike-Game/main/Resources/Resources.zip", FileSystem.AppDataDirectory + "//Resouces.zip"); }
-                try { Directory.Delete(FileSystem.AppDataDirectory + "//Data//Resources//", true); } //Deletes Resources folder and any subfolders
-                catch { Thread.Sleep(0); } // Does nothing, just prevents crash
+            await Task.Delay(1); //Delays task to make sure the play button updates
+            UpdaterStatus = "Requested Permissions";
+            using (var client = new WebClient()) { client.DownloadFile("https://github.com/Rarisma/YAG-Rougelike/raw/main/Resources/Resources.zip", FileSystem.AppDataDirectory + "//Resouces.zip"); }
 
-                ZipFile.ExtractToDirectory(FileSystem.AppDataDirectory + "//Resouces.zip", FileSystem.AppDataDirectory + "//Data//Resources//");
-                PlayButton.Text = "Update complete!";
-                await Task.Delay(3000);
-                PlayButton.Text = "Play";
-            }
-            catch //If somthing prevents the user from downloading resources.zip then it checks for resources 
-            {
-                if (Directory.Exists(FileSystem.AppDataDirectory + "//Data//Resources//")) // Checks if user already has resources folder
-                { await DisplayAlert("Resources Update failed!", "Couldn't connect to GitHub to download resources, you can continue however you might encounter errors/crashes.", "Ok"); } //allows user to continue if they already have /resources/
-                else { await DisplayAlert("Failed to find and update resources", "Couldn't connect to GitHub to download resources.\nCheck your internet connection and try again.", "Ok"); } //Tells user an error has occured
-            }
+            UpdaterStatus = "Attempting to download file";
+            try { Directory.Delete(FileSystem.AppDataDirectory + "//Data//Resources//", true); } //Deletes Resources folder and any subfolders
+            catch { UpdaterStatus = "Failed to delete //Resources// (Probably doesn't exist)"; } // Does nothing, just prevents crash
+
+            try { ZipFile.ExtractToDirectory(FileSystem.AppDataDirectory + "//Resouces.zip", FileSystem.AppDataDirectory + "//Data//Resources//"); }
+            catch { UpdaterStatus = "Zip Extraction Failed."; }
+
+            await Navigation.PushModalAsync(new GameView());
+            PlayButton.Text = "Update complete!";
+            await Task.Delay(1);
+            PlayButton.Text = "Play"; 
+
         }
     }
 }
